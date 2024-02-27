f99697410a60fe34ec9c04e3125fe81f56abb800
Dhruv Shetty <dhruvshetty2502@gmail.com>
2024-02-28 00:28:40 +0530

    recent commit

--- functions.js
+++ functions.js
@@ -1,23 +1,62 @@
 const galleryImages = {
     "activity 1": [
-      "img/picture27.png",
-      "img/picture28.png",
-      "img/picture29.png",
-      "img/picture30.png",
-      "img/picture31.png",
-      "img/picture32.png",
-      "img/picture33.png",
-      "img/picture34.png"
+      "img/picture27.jpg",
+      "img/picture28.jpg",
+      "img/picture29.jpg",
+      "img/picture30.jpg",
+      "img/picture31.jpg",
+      "img/picture32.jpg",
+      "img/picture33.jpg",
+      "img/picture34.jpg"
     ],
     "activity 2": [
-      "img/picture49.png",
-      "img/picture50.png",
-      "img/picture51.png",
-      "img/picture52.png",
-      "img/picture53.png",
-      "img/picture54.png",
-      "img/picture55.png",
-      "img/picture56.png"
+      "img/picture49.jpg",
+      "img/picture50.jpg",
+      "img/picture51.jpg",
+      "img/picture52.jpg",
+      "img/picture53.jpg",
+      "img/picture54.jpg",
+      "img/picture55.jpg",
+      "img/picture56.jpg"
+    ],
+    "activity 3": [
+      "img/picture62.jpg",
+      "img/picture63.jpg",
+      "img/picture64.jpg",
+      "img/picture65.jpg"
+    ],
+    "activity 4": [
+      "img/picture67.jpg",
+      "img/picture68.jpg",
+      "img/picture69.jpg"
+    ],
+    "activity 5": [
+      "img/picture20.jpg",
+      "img/picture21.jpg",
+      "img/picture22.jpg",
+      "img/picture23.jpg",
+      "img/picture24.jpg",
+      "img/picture25.jpg"
+    ],
+    "activity 6": [
+      "img/picture37.jpg",
+      "img/picture38.jpg",
+      "img/picture39.jpg",
+      "img/picture40.jpg",
+      "img/picture41.jpg",
+      "img/picture42.jpg"
+    ],
+    "activity 7": [
+      "img/picture70.jpg",
+      "img/picture71.jpg",
+      "img/picture46.jpg"
+    ],
+    "activity 8": [
+      "img/picture57.jpg",
+      "img/picture58.jpg",
+      "img/picture59.jpg",
+      "img/picture60.jpg",
+      "img/picture61.jpg"
     ],
   };
 
--- script.js
+++ script.js
@@ -30,16 +30,11 @@ const activities = {
     "date": "21-03-2021",
     "volunteer": "100",
     "images": [
-     "img/picture49.png",
-     "img/picture52.png",
-     "img/picture53.png",
+     "img/picture49.jpg",
+     "img/picture53.jpg",
+     "img/picture51.jpg",
      "img/nss3.jpeg"
-    ],
-    "Independence Day Celebration": [
-    "img/picture27.png",
-    "img/picture28.png",
-    "img/picture29.png"
-]
+    ]
   },
   "activity 2": {
     "name": "Republic Day Celebration",
@@ -48,27 +43,156 @@ const activities = {
     "date": "21-03-2021",
     "volunteer": "100",
     "images": [
-    "img/picture49.png",
-    "img/picture50.png",
-    "img/picture51.png",
+    "img/picture50.jpg",
+    "img/picture52.jpg",
+    "img/picture54.jpg",
     "img/nss12.jpeg"
     ]
+  },
+  "activity 3": {
+    "name": "Internationl Yoga Day",
+    "description": "SIESGST College joyously commemorates International Yoga Day, embracing the holistic essence of yoga for physical, mental, and spiritual well-being. Join us in a harmonious celebration that promotes balance and inner peace through engaging activities.<br><strong>➤ Yoga Sessions:</strong> Immerse yourself in rejuvenating yoga sessions led by experienced instructors, exploring various asanas and techniques to enhance flexibility, strength, and mindfulness. <br><strong>➤ Breathing Exercises:</strong> Delve into the art of conscious breathing with dedicated breathing exercises, fostering a connection between the mind and body, promoting relaxation and stress relief. <br><strong>➤ Meditation Workshops:</strong> Experience the transformative power of meditation in specialized workshops, guiding participants towards a state of calmness and self-awareness. <br><strong>➤ Yoga for Wellness Talks:</strong> Engage in enlightening discussions on the holistic benefits of yoga for overall well-being, including physical health, mental clarity, and emotional balance. ",
+    "row": "1",
+    "date": "21-03-2021",
+    "volunteer": "100",
+    "images": [
+    "img/picture62.jpg",
+    "img/picture63.jpg",
+    "img/picture64.jpg",
+    "img/nss5.jpeg"
+    ]
+  },
+  "activity 4": {
+    "name": "Mahatma Gandhi and Lal Bahadur Shastri Jayanti",
+    "description": "Join the NSS unit of SIESGST in commemorating the birth anniversaries of two stalwarts, Mahatma Gandhi and Lal Bahadur Shastri. Let's pay homage to their indomitable spirit and unwavering commitment to the principles of truth, non-violence, and service to the nation.<br><strong>➤ Inspirational Talks:</strong> Engage in enlightening discussions on the life and teachings of Mahatma Gandhi and Lal Bahadur Shastri, drawing inspiration from their leadership and vision for a harmonious society. <br><strong>➤ Tribute Ceremonies:</strong> Participate in solemn tribute ceremonies to honor the legacies of these great leaders, reflecting on their contributions to India's freedom struggle and nation-building. <br><strong>➤ Peace March:</strong> Embark on a symbolic peace march, embodying the principles of Gandhian philosophy, spreading the message of peace, unity, and communal harmony.",
+    "row": "1",
+    "date": "02-10-2021",
+    "volunteer": "150",
+    "images": [
+      "img/picture67.jpg",
+      "img/picture68.jpg",
+      "img/picture69.jpg",
+      "img/nss15.jpeg"
+    ]
+  },
+  
+  "activity 5": {
+    "name": "Mangrove Clean Drive",
+    "description": "Join hands with the NSS unit of SIESGST in a noble initiative to preserve and protect our coastal ecosystem. The Mangrove Clean Drive aims to contribute to environmental conservation by cleaning and maintaining the mangrove areas, crucial for biodiversity and ecological balance.<br><strong>➤ Mangrove Cleanup:</strong> Participate in hands-on cleanup activities to remove litter and debris from mangrove areas, promoting a cleaner and healthier environment. <br><strong>➤ Environmental Awareness:</strong> Engage in awareness campaigns about the importance of mangroves in supporting marine life, preventing soil erosion, and mitigating climate change. <br><strong>➤ Plantation Drive:</strong> Contribute to the reforestation efforts by planting mangrove saplings, fostering a sustainable and resilient coastal ecosystem.",
+    "row": "1",
+    "date": "15-06-2022",
+    "volunteer": "120",
+    "images": [
+      "img/picture24.jpg",
+      "img/picture23.jpg",
+      "img/picture22.jpg",
+      "img/nss16.jpeg"
+    ]
+  },
+  
+  "activity 6": {
+    "name": "Street Play",
+    "description": "Experience the power of storytelling and social messaging through the art of street play. The NSS unit of SIESGST invites you to witness and participate in vibrant street performances addressing relevant social issues, inspiring change, and fostering awareness in an entertaining way.<br><strong>➤ Creative Performances:</strong> Enjoy dynamic and thought-provoking street plays performed by talented volunteers, focusing on issues like environmental conservation, social justice, and community harmony. <br><strong>➤ Audience Interaction:</strong> Engage with the performers in interactive sessions, discussing the themes and encouraging dialogue on the presented social issues. <br><strong>➤ Expressive Art:</strong> Showcase your creativity by participating in the production or direction of street plays, using drama as a medium for social impact.",
+    "row": "1",
+    "date": "18-04-2022",
+    "volunteer": "80",
+    "images": [
+      "img/picture40.jpg",
+      "img/picture41.jpg",
+      "img/picture42.jpg",
+      "img/nss10.jpg"
+    ]
+  },
+  
+  "activity 7": {
+    "name": "Orphanage Visit",
+    "description": "Spread smiles and joy by joining the NSS unit of SIESGST in a heartwarming visit to an orphanage. This activity is an opportunity to connect with and bring happiness to children in need, creating memorable moments and fostering a sense of compassion and social responsibility.<br><strong>➤ Fun and Games:</strong> Engage in playful activities, games, and interactive sessions with the children, creating a positive and uplifting environment. <br><strong>➤ Gift Distribution:</strong> Contribute to the happiness of the children by distributing gifts, school supplies, and essentials, making a meaningful impact on their lives. <br><strong>➤ Cultural Performances:</strong> Share moments of joy through cultural performances, bringing smiles and laughter to the faces of the children.",
+    "row": "1",
+    "date": "12-11-2022",
+    "volunteer": "90",
+    "images": [
+      "img/picture70.jpg",
+      "img/picture71.jpg",
+      "img/picture46.jpg",
+      "img/nss14.jpeg"
+    ]
+  },
+  
+  "activity 8": {
+    "name": "Beach Clean Drive",
+    "description": "Be a steward of environmental conservation by participating in the Beach Clean Drive organized by the NSS unit of SIESGST. Contribute to the cleanliness and beauty of our coastal areas while raising awareness about the importance of keeping our beaches free from pollution.<br><strong>➤ Beach Cleanup:</strong> Roll up your sleeves and engage in hands-on cleanup activities, removing litter and plastic waste from the beach, ensuring a cleaner and safer environment. <br><strong>➤ Eco-friendly Practices:</strong> Promote eco-friendly practices and responsible waste disposal through educational campaigns and demonstrations during the event. <br><strong>➤ Community Involvement:</strong> Encourage community participation in maintaining the pristine beauty of our beaches for the benefit of present and future generations.",
+    "row": "1",
+    "date": "20-08-2023",
+    "volunteer": "130",
+    "images": [
+      "img/picture58.jpg",
+      "img/picture60.jpg",
+      "img/picture61.jpg",
+      "img/nss13.jpeg"
+    ]
   }
-  // Add more activities as needed
 };
 
 const galleryImages = {
   "activity 1": [
-    "img/picture27.png",
-    "img/picture28.png",
-    "img/picture29.png",
-    "img/picture30.png",
-    "img/picture31.png",
-    "img/picture32.png",
-    "img/picture33.png",
-    "img/picture34.png"
+    "img/picture27.jpg",
+    "img/picture28.jpg",
+    "img/picture29.jpg",
+    "img/picture30.jpg",
+    "img/picture31.jpg",
+    "img/picture32.jpg",
+    "img/picture33.jpg",
+    "img/picture34.jpg"
   ],
-  // Add more activities as needed
+  "activity 2": [
+    "img/picture49.jpg",
+    "img/picture50.jpg",
+    "img/picture51.jpg",
+    "img/picture52.jpg",
+    "img/picture53.jpg",
+    "img/picture54.jpg",
+    "img/picture55.jpg",
+    "img/picture56.jpg"
+  ],
+  "activity 3": [
+    "img/picture62.jpg",
+    "img/picture63.jpg",
+    "img/picture64.jpg",
+    "img/picture65.jpg"
+  ],
+  "activity 4": [
+    "img/picture67.jpg",
+    "img/picture68.jpg",
+    "img/picture69.jpg"
+  ],
+  "activity 5": [
+    "img/picture20.jpg",
+    "img/picture21.jpg",
+    "img/picture22.jpg",
+    "img/picture23.jpg",
+    "img/picture24.jpg",
+    "img/picture25.jpg"
+  ],
+  "activity 6": [
+    "img/picture37.jpg",
+    "img/picture38.jpg",
+    "img/picture39.jpg",
+    "img/picture40.jpg",
+    "img/picture41.jpg",
+    "img/picture42.jpg"
+  ],
+  "activity 7": [
+    "img/picture70.jpg",
+    "img/picture71.jpg",
+    "img/picture46.jpg"
+  ],
+  "activity 8": [
+    "img/picture57.jpg",
+    "img/picture58.jpg",
+    "img/picture59.jpg",
+    "img/picture60.jpg",
+    "img/picture61.jpg"
+  ],
 };
 
 function redirectToGallery() {
